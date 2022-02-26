9563c7eff89af7c803e61d60401940340aba7c98
marsellrumayauw <marsellino>
2022-02-26 13:36:32 +0900

    project03

---
+++ project03/comments.php
@@ -0,0 +1,20 @@
+<?php 
+
+    // this is my first single line commenty in php
+    echo "single line comment using '/' <br><br>";
+
+
+    # this is also a single line comment
+
+    #this is also
+
+    echo "single line comment using '#'";
+
+    /*
+        this is
+
+        multiple line comment 
+
+        in php
+
+?>
