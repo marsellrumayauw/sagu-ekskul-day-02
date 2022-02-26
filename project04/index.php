c1a9a94dea80b68ce2b8397c070bccd3cbfe4f83
marsellrumayauw <marsellino>
2022-02-26 13:48:56 +0900

    project04

---
+++ project04/index.php
@@ -0,0 +1,23 @@
+<?php 
+    
+    $firstname = Marcellino
+
+    $lastname = Rumayauw
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project04</title>
+</head>
+<body>
+    <div class="namadepan">
+        <?php echo $firstname; ?>
+    </div> 
+    <?php echo $firstname; ?> <?php echo $lastname; ?>
+</body>
+</html>
