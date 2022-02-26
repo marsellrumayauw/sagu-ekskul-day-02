8e57d8b7b85e47115f2b8865a42048e61a2c400b
marsellrumayauw <marsellino>
2022-02-26 12:48:57 +0900

    project01

---
+++ project01/php-and-html.php
@@ -0,0 +1,17 @@
+<?php 
+    print("
+    <html>
+
+    <head>
+        <title> PHP and HTML </title>
+    </head>
+
+    <body>
+    
+        <h1>HELLO SAGU!</h1>
+        
+        <p>We are learning PHP Codes.<p>
+
+    </body>
+    ");
+?>
